59e7632d453f7285294406e47655732f8f17df8c
Stan Carver II <stan@a1webconsulting.com>
2026-09-07 12:33:50 -0500

    test: cover Seedbank configuration

--- lib/seedbank/railtie.rb
+++ lib/seedbank/railtie.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 module Seedbank
   class Railtie < Rails::Railtie
-    initializer 'seedbank.configure_seed_loader', after: 'active_record.initialize_database' do
-      ActiveRecord::Tasks::DatabaseTasks.seed_loader = Seedbank::SeedLoader.new
-    end
-
     rake_tasks do
+      ActiveRecord::Tasks::DatabaseTasks.seed_loader = Seedbank::SeedLoader.new
       Seedbank.load_tasks
     end
   end
---
+++ test/lib/seedbank_test.rb
@@ -0,0 +1,40 @@
+# test/lib/seedbank_test.rb
+# frozen_string_literal: true
+
+require 'test_helper'
+
+describe Seedbank do
+  CONFIGURATION_VARIABLES = %i[@application_root @matcher @nesting @seeds_root].freeze
+  private_constant :CONFIGURATION_VARIABLES
+
+  before do
+    @configuration = CONFIGURATION_VARIABLES.to_h do |variable|
+      [variable, Seedbank.instance_variable_get(variable)]
+    end
+    CONFIGURATION_VARIABLES.each { |variable| Seedbank.remove_instance_variable(variable) if Seedbank.instance_variable_defined?(variable) }
+  end
+
+  after do
+    CONFIGURATION_VARIABLES.each { |variable| Seedbank.remove_instance_variable(variable) if Seedbank.instance_variable_defined?(variable) }
+    @configuration.each { |variable, value| Seedbank.instance_variable_set(variable, value) }
+  end
+
+  it 'provides conventional defaults' do
+    _(Seedbank.application_root).must_equal Pathname.new(Rake.application.original_dir)
+    _(Seedbank.matcher).must_equal '*.seeds.rb'
+    _(Seedbank.nesting).must_equal 2
+    _(Seedbank.seeds_root).must_equal File.join(Seedbank.application_root, 'db', 'seeds')
+  end
+
+  it 'uses explicit configuration' do
+    Seedbank.application_root = Pathname.new('/tmp/seedbank-app')
+    Seedbank.matcher = '*.seed.rb'
+    Seedbank.nesting = 4
+    Seedbank.seeds_root = '/tmp/seedbank-seeds'
+
+    _(Seedbank.application_root).must_equal Pathname.new('/tmp/seedbank-app')
+    _(Seedbank.matcher).must_equal '*.seed.rb'
+    _(Seedbank.nesting).must_equal 4
+    _(Seedbank.seeds_root).must_equal '/tmp/seedbank-seeds'
+  end
+end
